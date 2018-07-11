8152b94b1f6426440e7f0eacb6764878c9e4ebbf
Sosodope <sonadolkar93@hotmail.com>
2018-07-11 23:18:57 +0700

    set up express and fetch data

---
+++ client/src/App.js
@@ -0,0 +1,32 @@
+import React, { Component } from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import { link } from "fs";
+
+class App extends Component {
+  state = {
+    users: []
+  };
+  componentDidMount() {
+    fetch("/users")
+      .then(res => res.json())
+      .then(users => this.setState({ users }));
+  }
+  render() {
+    return (
+      <div className="App Wrapper">
+        <h1>Users</h1>
+        <div className="Contacts">
+          {this.state.users.map(user => (
+            <React.Fragment>
+              <h1 className="Username">{user.username}</h1>
+              <h5 className="Location">{user.location}</h5>
+            </React.Fragment>
+          ))}
+        </div>
+      </div>
+    );
+  }
+}
+
+export default App;
